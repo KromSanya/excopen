c5cf99f97f80eb811cb2e24da5682ebb05a41de8
MarkLevanovich999 <dalastexas999@gmail.com>
2025-03-01 11:47:55 +0600

    Update fix bug in form

--- client/src/features/searchTour/model/context/hooks.ts
+++ client/src/features/searchTour/model/context/hooks.ts
@@ -5,6 +5,5 @@ import {SearchContext} from "./context.ts";
 export const useSearchContext = (): SearchContextType => {
     const context = useContext(SearchContext)
     if (!context) throw new Error("Context error!")
-
     return context
 }
